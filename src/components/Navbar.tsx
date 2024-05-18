86d38ff27dcd569da92c4466355bba4c9b27c34c
Eedma <ed.marzorati@gmail.com>
2024-05-18 22:41:59 +0200

    navbar color fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,7 +50,7 @@ export default function Navbar() {
                             </Link>
                             <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
-                                <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-900 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-red-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                     <span className="absolute -inset-0.5" />
                                     <span className="sr-only">
                                         Open main menu
