0ee3c645c36bfb9e6f01911ed3a66fc5739604cf
Eedma <ed.marzorati@gmail.com>
2024-06-09 14:08:59 +0300

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import "./globals.css";
 import {Footer} from "../components/Footer";
 import Navbar from "@/components/Navbar";
+import {Analytics} from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
     title: "Raffaele Ciardulli",
@@ -22,6 +23,7 @@ export default function RootLayout({
                         {children}
                     </div>
                     <Footer />
+                    <Analytics />
                 </div>
             </body>
         </html>
