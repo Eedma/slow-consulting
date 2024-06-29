bc8731f3f7706620e5a3f6affff7b11ed0d1f215
Eedma <ed.marzorati@gmail.com>
2024-06-29 19:51:59 +0200

    iubenda provider cookie policy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,20 @@ import "./globals.css";
 import {Footer} from "../components/Footer";
 import Navbar from "@/components/Navbar";
 import {Analytics} from "@vercel/analytics/react";
+import {
+    IubendaProvider,
+    IubendaCookieSolutionBannerConfigInterface,
+} from "@mep-agency/next-iubenda";
 
 export const metadata: Metadata = {
     title: "Raffaele Ciardulli",
     description: "Slow Consulting Coach",
+};
+
+const iubendaBannerConfig: IubendaCookieSolutionBannerConfigInterface = {
+    siteId: 3687205,
+    cookiePolicyId: 20687957,
+    lang: "it",
 };
 
 export default function RootLayout({
@@ -17,14 +27,16 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                <div className="flex flex-col justify-between min-h-screen">
-                    <Navbar />
-                    <div className="flex-1 md:max-w-[960px] md:mx-auto content-center px-4 md:mt-[2rem]">
-                        {children}
+                <IubendaProvider bannerConfig={iubendaBannerConfig}>
+                    <div className="flex flex-col justify-between min-h-screen">
+                        <Navbar />
+                        <div className="flex-1 md:max-w-[960px] md:mx-auto content-center px-4 md:mt-[2rem]">
+                            {children}
+                        </div>
+                        <Footer />
+                        <Analytics />
                     </div>
-                    <Footer />
-                    <Analytics />
-                </div>
+                </IubendaProvider>
             </body>
         </html>
     );
