6eec6c82770f67ef5f77c1e06c4ddefa2c7e5bb6
Eedma <ed.marzorati@gmail.com>
2024-07-21 18:00:20 +0200

    nocookie banner

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,17 @@ export const Footer = () => {
                 width={130}
                 alt="slow consulting"
             />
-            <div>
-                {new Date().getFullYear()} Raffaele Ciardulli Slow Consulting
+            <div className="flex-1 md:max-w-[960px] md:mx-auto content-center px-4 md:mb-[2rem] text-center">
+                <h6>
+                    Questo sito rispetta la tua privacy e non utilizza cookie
+                    per il tracciamento o la memorizzazione delle tue
+                    informazioni personali. Naviga con tranquillità, sapendo che
+                    i tuoi dati sono al sicuro.
+                </h6>
+                <h6>
+                    &copy; {new Date().getFullYear()} Raffaele Ciardulli Slow
+                    Consulting
+                </h6>
             </div>
         </div>
     );
