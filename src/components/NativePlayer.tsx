8713bb88707e8c6871ff7402ef7356b6960d098e
Eedma <ed.marzorati@gmail.com>
2026-08-09 20:37:30 +0200

    feat: deploy fix

--- src/components/NativePlayer.tsx
+++ src/components/NativePlayer.tsx
@@ -3,7 +3,7 @@ export default function NativePlayer() {
     <iframe
       title="Retailer Strategy. Raffaele Ciardulli: lusso e visione, cosa unisce le due esperienze?"
       className="w-full aspect-video w-[80vw] h-[30vh] md:w-[560px] md:h-[315px]"
-      src="https://www.youtube.com/embed/7r6kCqSz4FE?si=q9wRJOHWQsuP7byu" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen
+      src="https://www.youtube.com/embed/7r6kCqSz4FE?si=q9wRJOHWQsuP7byu" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     />
   )
 }
